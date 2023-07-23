de6cd75484383b5dc04dbe7e0a001bcca25104bb
Pavel Plotnikov <paulplotnikovs@mail.ru>
2023-07-23 17:31:04 +0300

    Добавьте файлы проекта.

---
+++ WebAppCentralBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppCentralBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebAppCentralBank/Models/Parsing/Parsing.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppCentralBank.Models.Parsing
+{
+    class Parsing
+    {
+        const string PATHJSONCBRF = "https://www.cbr-xml-daily.ru/daily_json.js";
+        const string LIKEVALUEUSD = "USD";
+        const string LIKEVALUEEUR = "EUR";
+        const string LIKEVALUECNY = "CNY";
+
+    }
+}
